cbacb415c359288b80bfa3ed36a02032c62aa372
Neha Patel <neha.r.p@gmail.com>
2020-01-17 14:08:41 -0800

    cleaned up code/deleted print statments

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -16,20 +16,15 @@ def get_indices_of_item_weights(weights, length, limit):
     for i in range(0, len(weights)):
          #retrieve first weight
         weight1 = weights[i]
-        print("weight1", weight1)
          #subtract weight from limit
         weight2 = limit - weight1
-        print("weight2", weight2)
         weight2_in_table = hash_table_retrieve(ht, weight2)
-        print("weight2 in table", weight2_in_table)
         #check to see if the other weight exists in table
         if weight2_in_table is not None:
             #return tuple, larger weight at index 0
             if i > weight2_in_table:
-                print("return", i, weight2_in_table)
                 return (i, weight2_in_table)
             else:
-                print("return", weight2_in_table, i)
                 return (weight2_in_table, i)
         #if not, move to next weight
         i+=1
